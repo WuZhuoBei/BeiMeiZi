d73b4c1d5da6a955b2a4fba30b17797db099bab2
WuZhuoBei <1269800864@qq.com>
2019-07-11 13:29:39 +0800

    two

--- app/src/main/java/com/example/beimeizi/MainActivity.java
+++ app/src/main/java/com/example/beimeizi/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.beimeizi;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.beimeizi.utils.PictureLoader;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView imageview;
+    private Button btn;
+    private ArrayList<String> urls;
+    private int curPos = 0;
+    private PictureLoader loader= new PictureLoader();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //初始化控件
+        initView();
+        //访问地址数据
+        initData();
+    }
+
+    private void initData() {
+        urls = new ArrayList<String>();
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6ipaai7wgj20dw0kugp4.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6gcxc1t7vj20hs0hsgo1.jpg");
+        urls.add("http://ww4.sinaimg.cn/large/610dc034jw1f6f5ktcyk0j20u011hacg.jpg");
+        urls.add("http://ww1.sinaimg.cn/large/610dc034jw1f6e1f1qmg3j20u00u0djp.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f6aipo68yvj20qo0qoaee.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f69c9e22xjj20u011hjuu.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/610dc034jw1f689lmaf7qj20u00u00v7.jpg");
+        urls.add("http://ww3.sinaimg.cn/large/c85e4a5cjw1f671i8gt1rj20vy0vydsz.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/610dc034jw1f65f0oqodoj20qo0hntc9.jpg");
+        urls.add("http://ww2.sinaimg.cn/large/c85e4a5cgw1f62hzfvzwwj20hs0qogpo.jpg");
+    }
+
+    private void initView() {
+
+        imageview = (ImageView) findViewById(R.id.imageview);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn:
+                if(curPos>9){
+                    curPos = 0;
+                }
+                loader.load(imageview,urls.get(curPos));
+                curPos++;
+                break;
+        }
     }
 }
